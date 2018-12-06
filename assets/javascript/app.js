1f10e8a052c500fc6038c0f04a783f37cf7f1788
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2018-12-06 13:54:59 -0700

    new topic button working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,61 @@
+//topic array
 var topics = ["happy", "sad", "ecstatic", "angry", "jealous", "frustrated", "depressed"];
-var topic; 
-var imageURL;
-var gifURL;
-
-$(document).ready(function () {
+//gif gererator funciton
+function showButtons() {
+    $("#buttons").empty()
+    //for each item in the topics array, generate a button
     for (i = 0; i < topics.length; i++) {
         $("#buttons").append("<button class='buttons' id='" + topics[i] + "'>" + topics[i] + "</button>");
     }
-    $(".buttons").on("click", function getGifs() {
-        $("#images").html("");
-        topic = $(this).attr("id");
-        console.log(topic);
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=SMHcravp6J52lglHwZk6jPmm49li38p2" + "&q=" + topic + "&limit=10";
-        console.log(queryURL);
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            var results = response.data;
-            for(j = 0; j < results.length; j++) {
-                imageURL = results[j].images.fixed_height_still.url;
-                gifURL = results[j].images.fixed_height.url;
-                $("#images").append("<img src='" + imageURL + "' data-still='" + imageURL + "' data-animate='" + gifURL + "' data-state='still' class='gif'>");
-            }
-            $(".gif").on("click", function changeGifs() {
-                var state = $(this).attr("data-state");
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-                }
-            })
-        });
-    })
+}
+
+function getGifs() {
+    //clear our previous gifs
+    $("#images").html("");
+    var topic = $(this).attr("id");
+    //for (i = 0; i < 10; i++) {
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=SMHcravp6J52lglHwZk6jPmm49li38p2" + "&q=" + topic + "&limit=10";
+    console.log(queryURL);
+    //AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        //once the response in received function
+    }).then(function (response) {
+        console.log(response);
+        var results = response.data;
+        //put 10 gifs on the page
+        for (j = 0; j < results.length; j++) {
+            var imageURL = results[j].images.fixed_height_still.url;
+            var gifURL = results[j].images.fixed_height.url;
+            var rating = results[j].rating.toUpperCase();
+            $("#images").append("<div><img src='" + imageURL + "' data-still='" + imageURL + "' data-animate='" + gifURL + "' data-state='still' class='gif'><p>Rating: " + rating + "</p><div>");
+        }
+    });
+}
+
+//when a gif is clicked, animate it or deanimate it
+$(document).on("click", ".gif", function changeGifs() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 })
 
+$(document).on("click", "#addTerm", function (event) {
+    event.preventDefault();
+    var term = $("#term").val().trim();
+    topics.push(term);
+    $("#term").val("")
+    showButtons();
+})
 
+//when a button in clicked show 10 still image gifs
+$(document).on("click", ".buttons", getGifs)
+
+
+$(document).ready(showButtons);
